1f17f9cadaf1e45e079d7318df10d43a9414abe4
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-18 14:28:52 -0300

    build: inicia as dependencias

---
+++ src/main/java/com/medicaai/medicaai/MedicaaiApplication.java
@@ -0,0 +1,13 @@
+package com.medicaai.medicaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicaaiApplication.class, args);
+	}
+
+}
